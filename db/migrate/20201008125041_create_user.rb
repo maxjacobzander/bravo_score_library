9d971169a3baaf71b3e45da76aec0b56c3b96221
maxjacobzander <maxjacobzander@gmail.com>
2020-10-08 08:56:55 -0400

    reinitialization

---
+++ db/migrate/20201008125041_create_user.rb
@@ -0,0 +1,8 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+  end
+end
